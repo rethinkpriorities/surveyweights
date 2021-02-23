7b3b7af7c823df2d0f36b63372e15f61ebff9695
Peter Hurford <peter@peterhurford.com>
2021-02-23 11:24:04 -0600

    increment package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
      name='surveyweights',  
-     version='0.3',
+     version='0.4',
      author='Peter Hurford',
      author_email='peter@peterhurford.com',
      description='Apply Census weighting to survey data',
